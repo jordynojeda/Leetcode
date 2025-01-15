1d9ce52ac7c14c71466c1d58f5d1ff6fc0f26827
jordynojeda <65521566+jordynojeda@users.noreply.github.com>
2025-01-15 07:37:07 -0600

    Create 2429 - Minimize XOR.py

---
+++ 2429 - Minimize XOR.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        
+        def count_bits(n):
+            result = 0
+            while n > 0:
+
+                result += 1 & n
+
+                n = n >> 1
+            
+            return result
+
+        count1, count2 = count_bits(num1), count_bits(num2)
+        x = num1
+        i = 0
+
+        # Remove least significant bits
+        while count1 > count2:
+            if x & (1 << i):
+                count1 -= 1
+                x = x ^ (1 << i)
+
+            i += 1
+
+        # Adding least significant bits
+        while count1 < count2:
+            if x & (1 << i) == 0:
+                count1 += 1
+                x = x | (1 << i)
+
+            i += 1
+
+        return x
